3852f9c4d22cc62e8f698f95160111feb1583629
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-02-29 15:25:37 -0600

    Rewrite Dockerfile and docker_launch to enable container-only deploys
    Right now, users have to install terraform and ansible, and their
    dependencies, in order to user Mantl. Vagrant is a demo/test-drive
    option, but not for other use cases. After the installation(s), then
    they have to run multiple commands in order to get a cluster up.
    
    What I'm working towards is a way for users to be able to get started
    with Mantl in one or two steps, that removes the need to install
    dependenies. This commit reworks Dockerfile and docker_launch.sh, and in
    the tests that I have done, it can accomplish this.
    
    The ONBUILD statements are replaced with logic in
    docker_launch.sh to check for user-provided files in a particular place.
    If these files don't exist, then we copy/generate the files. (An
    exception to this is SSH keys.) When users mount a host volume in the
    MANTL_CONFIG_DIR, they will get copies of all of the generated files
    in that volume, which they are free to customize further.
    
    TODO: documentation, test without mounting preexisting mantl directory

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,9 @@ RUN pip install --upgrade pip \
 	&& pip install -r /mantl/requirements.txt
 
 VOLUME /local
-ENV TERRAFORM_VERSION=0.6.10 TERRAFORM_STATE_ROOT=/local
+ENV MANTL_CONFIG_DIR /local
 
+ENV TERRAFORM_VERSION=0.6.12 TERRAFORM_STATE_ROOT=$MANTL_CONFIG_DIR
 RUN mkdir -p /tmp/terraform/ && \
     cd /tmp/terraform/ && \
     curl -SLO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
@@ -17,4 +18,5 @@ RUN mkdir -p /tmp/terraform/ && \
     unzip /tmp/terraform/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
     rm -rf /tmp/terraform/
 
-WORKDIR /local
+WORKDIR /mantl
+CMD ./docker_launch.sh
--- docker_launch.sh
+++ docker_launch.sh
@@ -1,21 +1,47 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-SSH_KEY=${SSH_KEY:-"/root/.ssh/id_rsa"}
-
-if [ ! -f "$SSH_KEY" ]; then
-    mkdir -p /root/.ssh/
-    find /ssh/* ! -type l -print0 | xargs -0 cp -t /root/.ssh/
+if [ -z "$MANTL_PROVIDER" ]; then
+	echo "mantl.readthedocs.org for provider"
+	exit 1
 fi
-chown root:root /root/.ssh/*
+	
+if [ -z "$MANTL_KEY" ]; then
+	echo "mantl.readthedocs.org for key"
+	exit 1
+fi
 
-eval `ssh-agent -s` && ssh-add $SSH_KEY
+# prompt for SSH if not found
+SSH_KEY=$MANTL_CONFIG_DIR/$MANTL_KEY
+if [ ! -f "$SSH_KEY" ]; then
+	echo "mantl.readthedocs.org for ssh"
+fi
 
-if [ ! -f ./security.yml ]; then
-    ./security-setup --enable=false
+# gen security.yml if not found
+if [ ! -f $MANTL_CONFIG_DIR/security.yml ]; then
+	./security-setup --enable=false
+	cp security.yml $MANTL_CONFIG_DIR/security.yml
+else
+	cp /local/security.yml security.yml
+fi
+
+# copy sample.yml to mantl.yml if not found
+if [ ! -f $MANTL_CONFIG_DIR/mantl.yml ]; then
+	cp sample.yml mantl.yml
+	cp sample.yml $MANTL_CONFIG_DIR/mantl.yml
+else
+	cp $MANTL_CONFIG_DIR/mantl.yml mantl.yml
+fi
+
+# copy terraform sample file based on MANTL_PROVIDER env if user one doesn't exist
+if [ $(ls -l | grep .tf$ | wc -l) -gt 0 ]; then
+	cp /mantl/terraform/"$MANTL_PROVIDER".sample.tf mantl.tf
+	cp /mantl/terraform/"$MANTL_PROVIDER".sample.tf $MANTL_CONFIG_DIR/mantl.tf
+else
+	cp /local/*.tf /mantl/
 fi
 
 terraform get
-terraform apply -state=$TERRAFORM_STATE_ROOT/terraform.tfstate
-ansible-playbook /mi/playbooks/wait-for-hosts.yml --private-key $SSH_KEY
-ansible-playbook /mi/mantl.yml --extra-vars=@security.yml --private-key $SSH_KEY
+terraform apply
+ansible-playbook /mantl/playbooks/wait-for-hosts.yml --private-key $SSH_KEY
+ansible-playbook mantl.yml -e @"$MANTL_CONFIG_DIR/security.yml" --private-key $SSH_KEY
