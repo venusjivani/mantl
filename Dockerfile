28161e9f087cf12116ec51926f3f8729f08042f5
Sam Hatfield <samuel.e.hatfield@gmail.com>
2016-03-02 18:01:02 -0600

    Move Dockerfile, add entrypoint, and change scripts for convenience
    This commit switches the base image to alpine 3.3. Because of this, we
    need to install python and build tools to the image. It also adds
    `/bin/sh -c` as the entrypoint, to make this image easier to use on the
    command line.
    
    Convenience changes:
    - Add /root/.ssh as a volume
    - Change TERRAFORM_STATE_ROOT -> TERRAFORM_STATE
    - Don't copy files to mounted volume. This was an inconvenience

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,18 @@
-FROM python:2-alpine
+FROM alpine:3.3
 
-RUN apk add --no-cache build-base git openssh unzip
+RUN apk add --no-cache git openssh openssl python unzip
 
 RUN git clone https://github.com/CiscoCloud/mantl /mantl
 
-RUN pip install --upgrade pip \
-	&& pip install -r /mantl/requirements.txt
+RUN apk add --no-cache build-base python-dev py-pip \
+	&& pip install --upgrade pip \
+	&& pip install -r /mantl/requirements.txt \
+	&& apk del build-base python-dev py-pip
 
-VOLUME /local
+VOLUME /local /root/.ssh
 ENV MANTL_CONFIG_DIR /local
-ENV SSH_KEY /root/.ssh/id_rsa
 
-ENV TERRAFORM_VERSION=0.6.12 TERRAFORM_STATE_ROOT=$MANTL_CONFIG_DIR
+ENV TERRAFORM_VERSION=0.6.12 TERRAFORM_STATE=$MANTL_CONFIG_DIR/terraform.tfstate
 RUN mkdir -p /tmp/terraform/ && \
     cd /tmp/terraform/ && \
     curl -SLO https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
@@ -20,4 +21,9 @@ RUN mkdir -p /tmp/terraform/ && \
     rm -rf /tmp/terraform/
 
 WORKDIR /mantl
-CMD ./docker_launch.sh
+ENTRYPOINT ["/bin/sh", "-c"]
+CMD ["./docker_launch.sh"]
+# DOCS NEEDED:
+# copy over keys/certs if preexisting, otherwise generate ssh keys
+# copy over *.tf, mantl.yml, and security.yml if pre-existing
+# set env vars
--- docker_launch.sh
+++ docker_launch.sh
@@ -6,42 +6,36 @@ if [ -z "$MANTL_PROVIDER" ]; then
 	exit 1
 fi
 
-# prompt for SSH if not found
-mkdir -p $(dirname $SSH_KEY)
-if [ ! -f "$SSH_KEY" ]; then
-	ssh-keygen -N '' -f $SSH_KEY
-	cp $SSH_KEY* $MANTL_CONFIG_DIR/
+# SSH
+if [ ! -f "/root/.ssh/id_rsa" ]; then
+	ssh-keygen -N '' -f /root/.ssh/id_rsa
 else
 	cp $MANTL_CONFIG_DIR/$(basename $SSH_KEY) $(dirname $SSH_KEY)
 fi
-
 eval $(ssh-agent) && ssh-add $SSH_KEY
 
-# gen security.yml if not found
+# SECURITY.YML
 if [ ! -f $MANTL_CONFIG_DIR/security.yml ]; then
 	./security-setup --enable=false
-	cp security.yml $MANTL_CONFIG_DIR/security.yml
 else
 	cp /local/security.yml security.yml
 fi
 
-# copy sample.yml to mantl.yml if not found
+# MANTL.YML
 if [ ! -f $MANTL_CONFIG_DIR/mantl.yml ]; then
 	cp sample.yml mantl.yml
-	cp sample.yml $MANTL_CONFIG_DIR/mantl.yml
 else
 	cp $MANTL_CONFIG_DIR/mantl.yml mantl.yml
 fi
 
-# copy terraform sample file based on MANTL_PROVIDER env if user one doesn't exist
+# TERRAFORM FILES
 if [ "$(find $MANTL_CONFIG_DIR -name '*.tf')" == "" ]; then
 	cp /mantl/terraform/"$MANTL_PROVIDER".sample.tf mantl.tf
-	cp /mantl/terraform/"$MANTL_PROVIDER".sample.tf $MANTL_CONFIG_DIR/mantl.tf
 else
 	cp /local/*.tf /mantl/
 fi
 
-terraform get
-terraform apply
+terraform get -state=$TERRAFORM_STATE
+terraform apply -state=$TERRAFORM_STATE
 ansible-playbook /mantl/playbooks/wait-for-hosts.yml --private-key $SSH_KEY
 ansible-playbook mantl.yml -e @"$MANTL_CONFIG_DIR/security.yml" --private-key $SSH_KEY
